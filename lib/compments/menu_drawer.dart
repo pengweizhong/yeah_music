a0643956c03a16df07f975f8248aa06a73f37c69
pengweizhong <pengwz@hotmail.com>
2025-09-05 22:21:51 +0800

    调整抽屉

--- lib/compments/menu_drawer.dart
+++ lib/compments/menu_drawer.dart
@@ -13,50 +13,53 @@ class MenuDrawer extends StatefulWidget {
 class _MenuDrawerState extends State<MenuDrawer> {
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      child: Column(
-        //这里放一个Music的Logo
-        children: [
-          SizedBox(
-            height: 120,
-            width: 120,
-            child: DrawerHeader(child: Center(child: Image.asset("assets/icons/icon_512x512@2x.png"))),
-          ),
+    return SafeArea(
+      child: Drawer(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        child: Column(
+          //这里放一个Music的Logo
+          children: [
+            Container(
+              padding: EdgeInsetsGeometry.only(top: 30),
+              height: 180,
+              width: 180,
+              child: DrawerHeader(child: Center(child: Image.asset("assets/icons/icon_512x512@2x.png"))),
+            ),
 
-          //主页
-          Padding(
-            padding: EdgeInsetsGeometry.only(left: 90, right: 90, top: 20),
-            child: ListTile(title: Text("主页"), leading: Icon(Icons.home), onTap: () => Navigator.pop(context)),
-          ),
-          //设置
-          Padding(
-            padding: EdgeInsetsGeometry.only(left: 90, right: 90, top: 5),
-            child: ListTile(
-              title: Text("设置"),
-              leading: Icon(Icons.settings),
-              onTap: () {
-                //关闭当前页面 / 弹窗
-                Navigator.pop(context);
-                //导航到新的页面
-                Navigator.push(context, MaterialPageRoute(builder: (context) => SettingPage()));
-                //这种方式不保留当前页面栈 替换当前页面
-                // MaterialPageRoute(builder: (context) => ThemeSettings()),
-              },
+            //主页
+            Padding(
+              padding: EdgeInsetsGeometry.only(left: 90, right: 90, top: 20),
+              child: ListTile(title: Text("主页"), leading: Icon(Icons.home), onTap: () => Navigator.pop(context)),
             ),
-          ),
-          //关于
-          Padding(
-            padding: EdgeInsetsGeometry.only(left: 90, right: 90, top: 5),
-            child: ListTile(
-              title: Text("关于"),
-              leading: Icon(Icons.music_note),
-              onTap: () {
-                ApplicationUtils.showAboutDialog(context);
-              },
+            //设置
+            Padding(
+              padding: EdgeInsetsGeometry.only(left: 90, right: 90, top: 5),
+              child: ListTile(
+                title: Text("设置"),
+                leading: Icon(Icons.settings),
+                onTap: () {
+                  //关闭当前页面 / 弹窗
+                  Navigator.pop(context);
+                  //导航到新的页面
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SettingPage()));
+                  //这种方式不保留当前页面栈 替换当前页面
+                  // MaterialPageRoute(builder: (context) => ThemeSettings()),
+                },
+              ),
             ),
-          ),
-        ],
+            //关于
+            Padding(
+              padding: EdgeInsetsGeometry.only(left: 90, right: 90, top: 5),
+              child: ListTile(
+                title: Text("关于"),
+                leading: Icon(Icons.music_note),
+                onTap: () {
+                  ApplicationUtils.showAboutDialog(context);
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
